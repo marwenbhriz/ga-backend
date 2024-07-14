2113d4762dd78859d096ea5528dd0add57d78550
Marwen Ben Hriz <142993710+marwenbhriz@users.noreply.github.com>
2024-07-14 21:22:58 +0900

    add book endpoints

--- main.go
+++ main.go
@@ -5,17 +5,25 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/marwenbhriz/ga-backend/controllers/apicontroller"
+	"github.com/marwenbhriz/ga-backend/controllers/bookcontroller"
 )
 
 func main() {
 
 	router := gin.Default()
 
-	log.Println("Books API start listen on port 8081.")
+	log.Println("Books API start listen on port 8085.")
 
 	// apicontroller routes
 	router.GET("/api", apicontroller.Index)
 
-	router.Run(":8081")
+	// books routes
+	router.GET("/api/books", bookcontroller.Index)
+	router.GET("/api/book/:id", bookcontroller.Show)
+	router.POST("/api/book", bookcontroller.Create)
+	router.PUT("/api/book/:id", bookcontroller.Update)
+	router.DELETE("/api/book", bookcontroller.Delete)
+
+	router.Run(":8085")
 
 }
