26c3246da95e183d0029a3c808b96a3cc46b65ce
Marwen Ben Hriz <142993710+marwenbhriz@users.noreply.github.com>
2024-07-16 17:21:13 +0900

    cleanup code

--- main.go
+++ main.go
@@ -1,71 +1,194 @@
 package main
 
 import (
-	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/handlers"
-	"github.com/marwenbhriz/ga-backend/controllers/apicontroller"
-	"github.com/marwenbhriz/ga-backend/controllers/usercontroller"
-	"github.com/marwenbhriz/ga-backend/models"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	Routers()
+}
 
-	router := gin.Default()
+func Routers() {
+	InitDB()
+	defer db.Close()
+	log.Println("Starting the HTTP server on port 9090")
+	router := mux.NewRouter()
+	router.HandleFunc("/users",
+		GetUsers).Methods("GET")
+	router.HandleFunc("/users",
+		CreateUser).Methods("POST")
+	router.HandleFunc("/users/{id}",
+		GetUser).Methods("GET")
+	router.HandleFunc("/users/{id}",
+		UpdateUser).Methods("PUT")
+	router.HandleFunc("/users/{id}",
+		DeleteUser).Methods("DELETE")
+	http.ListenAndServe(":9090",
+		&CORSRouterDecorator{router})
+}
 
-	models.ConnectDatabase()
+/***************************************************/
 
-	log.Println("Tasks API start listen on port 8086.")
-
-	// apicontroller routes
-	router.GET("/api", apicontroller.Index)
-
-	// tasks routes
-	router.GET("/api/users", usercontroller.Index)
-	router.GET("/api/user/:id", usercontroller.Show)
-	router.POST("/api/user", usercontroller.Create)
-	router.PUT("/api/user/:id", usercontroller.Update)
-	router.DELETE("/api/user", usercontroller.Delete)
-
-	// cors middleware
-	methods := handlers.AllowedMethods([]string{"OPTIONS", "DELETE", "GET", "HEAD", "POST", "PUT"})
-	origins := handlers.AllowedOrigins([]string{"*"})
-	headers := handlers.AllowedHeaders([]string{"Content-Type"})
-	handler := handlers.CORS(methods, origins, headers)(router)
-
-	// create and start server
-	s := &http.Server{
-		Addr:     ":8086",       // bind address
-		Handler:  handler,       // default handler
-		ErrorLog: log.Default(), // logger for the server
+// Get all users
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	result, err := db.Query("SELECT id, first_name," +
+		"last_name,email from users")
+	if err != nil {
+		panic(err.Error())
 	}
-	go func() {
-		log.Println("Listening on port 8086")
-		log.Fatal(s.ListenAndServe())
-	}()
+	defer result.Close()
+	for result.Next() {
+		var user User
+		err := result.Scan(&user.ID, &user.FirstName,
+			&user.LastName, &user.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
+}
 
-	// trap terminate or interrupt
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, syscall.SIGTERM)
+// Create user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	stmt, err := db.Prepare("INSERT INTO users(first_name," +
+		"last_name,email) VALUES(?,?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	first_name := keyVal["firstName"]
+	last_name := keyVal["lastName"]
+	email := keyVal["email"]
+	_, err = stmt.Exec(first_name, last_name, email)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "New user was created")
+}
 
-	// block shutdown until terminal/interrupt signal received
-	// ⛔️ DON'T DELETE! This will shutdown server immediately
-	sig := <-c
-	log.Println("Gracefully shutting down...", sig)
+// Get user by ID
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT id, first_name,"+
+		"last_name,email from users WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var user User
+	for result.Next() {
+		err := result.Scan(&user.ID, &user.FirstName,
+			&user.LastName, &user.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(user)
+}
 
-	// gracefully shutdown server, waiting max 30 seconds for current operations to complete
-	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	cancel()
-	s.Shutdown(tc)
+// Update user
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("UPDATE users SET first_name = ?," +
+		"last_name= ?, email=? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	first_name := keyVal["firstName"]
+	last_name := keyVal["lastName"]
+	email := keyVal["email"]
+	_, err = stmt.Exec(first_name, last_name, email,
+		params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "User with ID = %s was updated",
+		params["id"])
+}
 
-	//router.Run(":8096")
+// Delete User
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM users WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "User with ID = %s was deleted",
+		params["id"])
+}
 
+/***************************************************/
+
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+}
+
+// Db configuration
+var db *sql.DB
+var err error
+
+func InitDB() {
+	db, err = sql.Open("mysql",
+		"root:password@tcp(34.146.47.61:3306)/userdb")
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+/***************************************************/
+
+// CORSRouterDecorator applies CORS headers to a mux.Router
+type CORSRouterDecorator struct {
+	R *mux.Router
+}
+
+func (c *CORSRouterDecorator) ServeHTTP(rw http.ResponseWriter,
+	req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods",
+			"POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Accept-Language,"+
+				" Content-Type, YourOwnHeader")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+
+	c.R.ServeHTTP(rw, req)
 }
