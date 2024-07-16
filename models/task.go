72cb7714c0f948298ff0f6fc8dff86fa6878245e
Marwen Ben Hriz <142993710+marwenbhriz@users.noreply.github.com>
2024-07-16 22:06:00 +0900

    cleanup

--- models/task.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID        int64  `gorm:"primaryKey" json:"id"`
-	FirstName string `gorm:"type:varchar(300)" json:"firstName"`
-	LastName  string `gorm:"type:varchar(300)" json:"lastName"`
-	Email     string `gorm:"type:varchar(300)" json:"email"`
-}
